e207c0455dea79189236c1bfb8e52c20b5b29cee
dbteku <dbteku@hotmail.com>
2018-07-08 00:52:25 -0600

    Catching parsing exception in Login

--- src/main/java/com/dbteku/fileserver/api/controllers/AuthControllerV1.java
+++ src/main/java/com/dbteku/fileserver/api/controllers/AuthControllerV1.java
@@ -6,8 +6,10 @@ import static spark.Spark.post;
 import com.dbteku.fileserver.api.services.IAuthServiceV1;
 import com.dbteku.fileserver.models.NewUser;
 import com.dbteku.fileserver.responses.HttpResponse;
+import com.dbteku.fileserver.responses.InvalidJsonResponse;
 import com.dbteku.fileserver.tools.Authorization;
 import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
 
 import spark.Request;
 import spark.Response;
@@ -68,64 +70,64 @@ public class AuthControllerV1 implements IAuthControllerV1{
 		//			return gson.toJson(response);
 		//		});
 
-//		delete("/v1/auth/:user", (req, res)->{
-			//			HttpResponse response = new OkResponse();
-			//			setJsonHeaders(res);
-			//			String id = req.headers(AUTH_HEADER);
-			//			if(isLoggedIn(id)) {
-			//				String username = req.params(":user");
-			//				if(clients.has(username)) {
-			//					ClientSession session = sessions.get(id);
-			//					if(session.getUsername().equals(username)) {
-			//						response = new CannotDeleteYourselfResponse();
-			//						res.status(400);
-			//					}else {
-			//						clients.delete(username);	
-			//					}
-			//				}else {
-			//					response = new NotFoundResponse();
-			//					res.status(400);
-			//				}
-			//			}else {
-			//				response = new UnAuthorizedResponse();
-			//				res.status(401);
-			//			}
-			//			return gson.toJson(response);
-//		});
+		//		delete("/v1/auth/:user", (req, res)->{
+		//			HttpResponse response = new OkResponse();
+		//			setJsonHeaders(res);
+		//			String id = req.headers(AUTH_HEADER);
+		//			if(isLoggedIn(id)) {
+		//				String username = req.params(":user");
+		//				if(clients.has(username)) {
+		//					ClientSession session = sessions.get(id);
+		//					if(session.getUsername().equals(username)) {
+		//						response = new CannotDeleteYourselfResponse();
+		//						res.status(400);
+		//					}else {
+		//						clients.delete(username);	
+		//					}
+		//				}else {
+		//					response = new NotFoundResponse();
+		//					res.status(400);
+		//				}
+		//			}else {
+		//				response = new UnAuthorizedResponse();
+		//				res.status(401);
+		//			}
+		//			return gson.toJson(response);
+		//		});
 
 		post("/v1/auth/login", (req, res)->{
 			return gson.toJson(login(req, res));
-					//			HttpResponse response = new OkResponse();
-					//			setJsonHeaders(res);
-					//			NewUser user = gson.fromJson(req.body(), NewUser.class);
-					//			if(user == null) {
-					//				response = new InvalidJsonResponse();
-					//				res.status(400);
-					//			}else {
-					//				ProtectedUser protectedUser = clients.get(user.getUsername());
-					//				if(protectedUser.isNull()) {
-					//					response = new InvalidUsernameOrPassword();
-					//				}else {
-					//					String potentialSaltedPassword = user.getPassword() + protectedUser.getSaltCode();
-					//					Sha256Encryption encryption = new Sha256Encryption();
-					//					String potentialPassword = encryption.encrypt(potentialSaltedPassword);
-					//					if(potentialPassword.equals(protectedUser.getHashedPassword())) {
-					//						String sessionId = UUID.randomUUID().toString();
-					//						while(sessions.has(sessionId)) {
-					//							sessionId = UUID.randomUUID().toString();
-					//						}
-					//						long now = System.currentTimeMillis();
-					//						long expire = now + (ONE_SECOND_IN_MILLIS * 60 * 30);
-					//						ClientSession session = new ClientSession(sessionId, protectedUser.getUsername(), expire);
-					//						sessions.put(session.getId(), session);
-					//						response = new LoggedInResponse(session);
-					//					}else {
-					//						response = new InvalidUsernameOrPassword();
-					//					}
-					//				}
-					//			}
-					//
-					//			return gson.toJson(response);
+			//			HttpResponse response = new OkResponse();
+			//			setJsonHeaders(res);
+			//			NewUser user = gson.fromJson(req.body(), NewUser.class);
+			//			if(user == null) {
+			//				response = new InvalidJsonResponse();
+			//				res.status(400);
+			//			}else {
+			//				ProtectedUser protectedUser = clients.get(user.getUsername());
+			//				if(protectedUser.isNull()) {
+			//					response = new InvalidUsernameOrPassword();
+			//				}else {
+			//					String potentialSaltedPassword = user.getPassword() + protectedUser.getSaltCode();
+			//					Sha256Encryption encryption = new Sha256Encryption();
+			//					String potentialPassword = encryption.encrypt(potentialSaltedPassword);
+			//					if(potentialPassword.equals(protectedUser.getHashedPassword())) {
+			//						String sessionId = UUID.randomUUID().toString();
+			//						while(sessions.has(sessionId)) {
+			//							sessionId = UUID.randomUUID().toString();
+			//						}
+			//						long now = System.currentTimeMillis();
+			//						long expire = now + (ONE_SECOND_IN_MILLIS * 60 * 30);
+			//						ClientSession session = new ClientSession(sessionId, protectedUser.getUsername(), expire);
+			//						sessions.put(session.getId(), session);
+			//						response = new LoggedInResponse(session);
+			//					}else {
+			//						response = new InvalidUsernameOrPassword();
+			//					}
+			//				}
+			//			}
+			//
+			//			return gson.toJson(response);
 		});
 
 		post("/v1/auth/logout", (req, res)->{
@@ -134,7 +136,11 @@ public class AuthControllerV1 implements IAuthControllerV1{
 	}
 	@Override
 	public HttpResponse login(Request req, Response res) {
-		HttpResponse response = service.login(gson.fromJson(req.body(), NewUser.class));
+		HttpResponse response = new InvalidJsonResponse();
+		try {
+			response = service.login(gson.fromJson(req.body(), NewUser.class));
+		}catch(JsonParseException e) {
+		}
 		res.status(response.getStatusCode());
 		return response;
 	}
