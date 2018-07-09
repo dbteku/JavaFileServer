07dd3d5e56125bbe5566d5e013ef3ba30a291a07
dbteku <dbteku@hotmail.com>
2018-07-09 14:23:22 -0600

    Removing obsolete code

--- src/main/java/com/dbteku/fileserver/api/controllers/AuthControllerV1.java
+++ src/main/java/com/dbteku/fileserver/api/controllers/AuthControllerV1.java
@@ -28,16 +28,6 @@ public class AuthControllerV1 implements IAuthControllerV1{
 	public void start() {
 		get("/v1/auth/loggedIn", (req, res)->{
 			return isLoggedIn(req, res);
-			//			HttpResponse response = new OkResponse();
-			//			setJsonHeaders(res);
-			//			String id = req.headers(AUTH_HEADER);
-			//			if(isLoggedIn(id)) {
-			//				response = new LoggedInResponse(sessions.get(id));
-			//			}else {
-			//				response = new UnAuthorizedResponse();
-			//				res.status(401);
-			//			}
-			//			return gson.toJson(response);
 		});
 
 		//		post("/v1/auth/user", (req, res)->{
@@ -97,37 +87,6 @@ public class AuthControllerV1 implements IAuthControllerV1{
 
 		post("/v1/auth/login", (req, res)->{
 			return gson.toJson(login(req, res));
-			//			HttpResponse response = new OkResponse();
-			//			setJsonHeaders(res);
-			//			NewUser user = gson.fromJson(req.body(), NewUser.class);
-			//			if(user == null) {
-			//				response = new InvalidJsonResponse();
-			//				res.status(400);
-			//			}else {
-			//				ProtectedUser protectedUser = clients.get(user.getUsername());
-			//				if(protectedUser.isNull()) {
-			//					response = new InvalidUsernameOrPassword();
-			//				}else {
-			//					String potentialSaltedPassword = user.getPassword() + protectedUser.getSaltCode();
-			//					Sha256Encryption encryption = new Sha256Encryption();
-			//					String potentialPassword = encryption.encrypt(potentialSaltedPassword);
-			//					if(potentialPassword.equals(protectedUser.getHashedPassword())) {
-			//						String sessionId = UUID.randomUUID().toString();
-			//						while(sessions.has(sessionId)) {
-			//							sessionId = UUID.randomUUID().toString();
-			//						}
-			//						long now = System.currentTimeMillis();
-			//						long expire = now + (ONE_SECOND_IN_MILLIS * 60 * 30);
-			//						ClientSession session = new ClientSession(sessionId, protectedUser.getUsername(), expire);
-			//						sessions.put(session.getId(), session);
-			//						response = new LoggedInResponse(session);
-			//					}else {
-			//						response = new InvalidUsernameOrPassword();
-			//					}
-			//				}
-			//			}
-			//
-			//			return gson.toJson(response);
 		});
 
 		post("/v1/auth/logout", (req, res)->{
